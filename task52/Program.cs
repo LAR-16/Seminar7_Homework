a772ba400df3764d762a5610a419f1f1c3fa5f7b
Alexandra <lebedeva1618@yandex.ru>
2022-12-21 14:28:34 +0300

    задача 52, среднее арифметическое каждого столбца

--- task52/Program.cs
+++ task52/Program.cs
@@ -1,6 +1,4 @@
-﻿// НЕПРАВИЛЬНО считает. переделать
-
-void InputMatrix(int[,] matrix)
+﻿void InputMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -13,18 +11,6 @@ void InputMatrix(int[,] matrix)
     }
 }
 
-// void FindAverage(int[,] matrix)
-// {
-//     double sum = 0;
-//     for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             for (int i = 0; i < matrix.GetLength(0); i++)
-//             {
-//             sum = sum + matrix[i, j];
-//             }
-//             Console.WriteLine(sum/size[0]);
-//     }
-// }
 
 Console.Clear();
 Console.Write("Введите размер матрицы: ");
@@ -32,13 +18,14 @@ int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
 Console.WriteLine();
-// FindAverage(matrix);
-double sum = 0;
-    for (int j = 0; j < matrix.GetLength(1); j++)
+Console.Write ("Среднее арифметическое каждого столбца: ");
+for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
         {
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-            sum = sum + matrix[i, j];
-            }
-            Console.WriteLine(Math.Round(sum/size[0], 2));
-    }
+        sum = sum + matrix[i, j];
+        }
+        Console.Write($"{Math.Round(sum/size[0], 2)}; ");
+    } 
+
