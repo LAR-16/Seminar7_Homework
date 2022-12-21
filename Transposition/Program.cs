0d04f16ea436bde57acf3703d693f36005893061
Alexandra <lebedeva1618@yandex.ru>
2022-12-21 17:36:43 +0300

    отражение матрицы относительно горизонтали

---
+++ Transposition/Program.cs
@@ -0,0 +1,48 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 101);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0)/2; i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int n = 0;
+            n = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) -1- i, j];
+            matrix[matrix.GetLength(0) -1- i, j] = n;
+        }
+    }
+}
+
+void PrintMatrix(int[,]matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine();
+ChangeMatrix(matrix);
+PrintMatrix(matrix);
